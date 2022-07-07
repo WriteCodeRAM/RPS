da3a97a20b3ab0fbf6d325329a2a34be1459ab98
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2022-07-07 12:32:41 -0400

    cleaned up the code

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const choices = ['rock', 'paper', 'scissors'];
 
-let winningScore = 3;
+let winningScore = 5;
 let playerScore = 0;
 let computerScore = 0;
 let tieCounter = 0;
-
-let computerSelection = computerPlay();
-let playerSelection;
-
-// const PLAYER_WIN = `CONGRATS, you win! you chose ${playerSelection} and the computer chose ${computerSelection}`;
-// const COMPUTER_WIN = `You chose ${playerSelection} and the computer chose ${computerSelection}, You LOSE!`;
-// const TIE_GAME = ` You both chose ${playerSelection}, it's a TIE!`;
 
 let PLAYER_WON = `You have beat the computer in a 5 round game of Rock, Paper, Scissors!`;
 let COMPUTER_WON = `You have lost to the computer in a 5 round game of Rock, Paper, Scissors! `;
@@ -21,36 +14,10 @@ function computerPlay() {
   return computerChoice;
 }
 
-function playerPlay() {
-  //validate input
-  let isValid = false;
-  let playerChoice;
-  while (!isValid) {
-    playerChoice = prompt('Enter Rock, Paper, or Scissors').toLowerCase();
-    if (
-      playerChoice !== 'rock' &&
-      playerChoice !== 'paper' &&
-      playerChoice !== 'scissors'
-    ) {
-      playerChoice = prompt(
-        'Enter Rock, Paper, or Scissors. Spelling matters but it is not case sensitive'
-      );
-      playerChoice.toLowerCase();
-    } else {
-      isValid = true;
-    }
-  }
-
-  return playerChoice;
-}
-
-// Keep it simple, we'll make a similar function getting the players choice
-// then calling both in a 'game' function and compare the results
-
 function playRound(playerSelection, computerSelection) {
   computerSelection = computerPlay();
 
-  while (playerScore < 5 && computerScore < 5) {
+  while (playerScore < winningScore && computerScore < winningScore) {
     if (
       (playerSelection === 'rock' && computerSelection === 'scissors') ||
       (playerSelection === 'paper' && computerSelection === 'rock') ||
@@ -58,7 +25,7 @@ function playRound(playerSelection, computerSelection) {
     ) {
       playerScore++;
       playerScoreboard.innerText = playerScore;
-      if (playerScore === 5) {
+      if (playerScore === winningScore) {
         resultsDisplay.innerText = PLAYER_WON;
         disableButtons();
         return;
@@ -71,7 +38,7 @@ function playRound(playerSelection, computerSelection) {
     } else {
       computerScore++;
       computerScoreboard.innerText = computerScore;
-      if (computerScore === 5) {
+      if (computerScore === winningScore) {
         resultsDisplay.innerText = COMPUTER_WON;
         disableButtons();
         return;
@@ -81,29 +48,15 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-// console.log(playRound(playerPlay(), computerPlay()));
+function disableButtons() {
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+}
 
-const buttons = document.querySelectorAll('button');
-
+const buttons = document.querySelectorAll('button'); // rock, paper, scissors
 const btnHolder = document.querySelector('.button-holder'); // div holding btns
 
-// add an event listener that selects 'rock' if the player clicks rock and so on..
-//learn foreach
-//apply logic to all buttons (CHECK)
-
-const rockBtn = document.querySelector('.rock');
-
-// buttons.forEach((button) => {
-//   button.addEventListener('click', (e) => {
-//     let playerSelect = e.target.innerText.toLowerCase();
-
-//     while (playerScore < 5 && computerScore < 5) {
-//       playRound(playerSelect, computerPlay());
-//     }
-//   });
-// });
-
-//HMMMM
 //display results outside of the console
 const resultsDisplay = document.querySelector('.resultsDisplay');
 
@@ -112,20 +65,9 @@ const playerScoreboard = document.querySelector('.playerScore');
 const computerScoreboard = document.querySelector('.computerScore');
 const tieScoreboard = document.querySelector('.tieScore');
 
-//failed
-// playerScoreboard.append(playerScore);
-// computerScoreboard.append(computerScore);
-
-//game ends when player or cpu reaches score of 5
-
+//calls the playround function with the button the user clicks
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
     playRound(e.target.innerText.toLowerCase());
   });
 });
-
-function disableButtons() {
-  buttons.forEach((button) => {
-    button.disabled = true;
-  });
-}
